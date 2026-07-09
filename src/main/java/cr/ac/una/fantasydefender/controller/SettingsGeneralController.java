ca7b424e72b0c2b95e73cdd761cde295eee8db94
orewatakka <fantasydefender.support@gmail.com>
2026-07-09 11:51:17 -0600

    Implementnacino de GeneralSettings

--- src/main/java/cr/ac/una/fantasydefender/controller/SettingsGeneralController.java
+++ src/main/java/cr/ac/una/fantasydefender/controller/SettingsGeneralController.java
@@ -1,15 +1,26 @@
 package cr.ac.una.fantasydefender.controller;
 
+import io.github.palexdev.materialfx.controls.MFXRadioButton;
+import io.github.palexdev.materialfx.controls.MFXSlider;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
- * FXML Controller class
  *
  * @author takkasama
  */
 public class SettingsGeneralController  extends Controller implements Initializable {
+
+    @FXML
+    private MFXSlider slGlobalVolume;
+    @FXML
+    private MFXSlider slSoundsEffectVolume;
+    @FXML
+    private MFXSlider slMusicVolume;
+    @FXML
+    private MFXRadioButton rdFullscreen;
 
     /**
      * Initializes the controller class.
